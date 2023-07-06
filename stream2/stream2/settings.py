5835ccfcecc07b407b0cc1ff3370aa84fd16440a
vazik66 <vasilkov.dev@gmail.com>
2023-07-06 13:37:01 +0500

    Added postgresql settings

--- stream2/stream2/settings.py
+++ stream2/stream2/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'stream2.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "music_app",
+        "USER": "postgres",
+        "PASSWORD": "mysecretpassword",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
