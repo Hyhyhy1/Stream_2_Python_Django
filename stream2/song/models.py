7a8a486af7f4bac23818463f783639e5e8126e88
vazik66 <vasilkov.dev@gmail.com>
2023-07-07 13:49:40 +0500

    added song comments

--- stream2/song/models.py
+++ stream2/song/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from artist.models import Artist
+from user.models import User
 
 
 class Song(models.Model):
@@ -28,3 +29,15 @@ class Song(models.Model):
     
     def __str__(self) -> str:
         return self.name
+    
+
+class SongComments(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    comment = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return f"comment from {self.user_id}"
