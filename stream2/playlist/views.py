8f22f8a5271d8ee9044832ab5a91eab2a2a9d09b
Hyhyhy1 <kuleshov.nikita@urfu.me>
2023-07-11 08:03:14 +0500

    most song endpoints added

--- stream2/playlist/views.py
+++ stream2/playlist/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Playlist
 # Create your views here.
+class playlistApiView(APIView):
+    def get(self, request):
+        lst = Playlist.objects.all().values()
+        return Response({'data': list(lst)})
