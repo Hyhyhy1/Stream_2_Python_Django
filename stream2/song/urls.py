42c9c9e3a3b30b73b8ccd3513e9fd6b97b501d0c
Hyhyhy1 <kuleshov.nikita@urfu.me>
2023-07-11 12:32:23 +0500

    song upload added

--- stream2/song/urls.py
+++ stream2/song/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from song.views import SongList, SongAPI, SongCreator
+from song.views import SongList, SongAPI, SongCreator, FileUploadView
 
 
 urlpatterns = [
     path('api/song/', SongList.as_view()),
     path('api/song/<int:pk>/', SongAPI.as_view()),
     path('api/song/create/', SongCreator.as_view()),
+    path('api/song/upload/', FileUploadView.as_view(), name='file-upload'),
+
 ]
